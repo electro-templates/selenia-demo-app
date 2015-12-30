658dc238ea7ffc8c8913b13ede7ae2a4f85229a2
Cláudio Silva <claudio.silva@impactwave.com>
2015-12-30 20:15:57 +0000

    Starting to upgrade to newest framework version.

---
+++ src/Config/DemoAppModule.php
@@ -0,0 +1,72 @@
+<?php
+namespace SeleniaTemplates\DemoApp\Config;
+
+use App\Welcome\Controllers\Index;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Selenia\Core\Assembly\Services\ModuleServices;
+use Selenia\Http\Components\PageComponent;
+use Selenia\Interfaces\Http\RouterInterface;
+use Selenia\Interfaces\ModuleInterface;
+use Selenia\Interfaces\Navigation\NavigationInterface;
+use Selenia\Interfaces\Navigation\NavigationProviderInterface;
+
+class DemoAppModule implements ModuleInterface, NavigationProviderInterface
+{
+  /** @var RouterInterface */
+  private $router;
+
+  function __invoke (ServerRequestInterface $request, ResponseInterface $response, callable $next)
+  {
+    return $this->router
+      ->set ([
+
+        // Example route implementing a self-contained component-like controller.
+
+        '.' => Index::class,
+
+        // Example route using an automatic controller and an external view.
+
+        'example' => factory (function (PageComponent $page) {
+          $page->templateUrl = 'index.html';
+          return $page;
+        }),
+
+      ])
+      ->__invoke ($request, $response, $next);
+  }
+
+  function configure (ModuleServices $module, RouterInterface $router)
+  {
+    $this->router = $router;
+    $module
+      ->publishPublicDirAs ('modules/selenia/welcome')
+      ->provideMacros ()
+      ->provideViews ()
+      ->registerRouter ($this)
+      ->provideNavigation ($this)
+      ->setDefaultConfig ([
+        'main' => [
+          'name'    => 'site',              // session cookie name
+          'appName' => 'Your App',
+          'title'   => '@ - Your App',      // @ = page title
+        ],
+      ]);
+  }
+
+  function defineNavigation (NavigationInterface $navigation)
+  {
+    $navigation->add ([
+      '' => $navigation
+        ->link ()
+        ->id ('home')
+        ->title ('Welcome')
+        ->links ([
+          'example' => $navigation
+            ->link ()
+            ->title ('Example'),
+        ]),
+    ]);
+  }
+
+}
